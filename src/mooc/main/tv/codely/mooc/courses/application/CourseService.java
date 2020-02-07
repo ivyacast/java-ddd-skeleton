b100dfa095f93d96b4b7340e4ff915ec00a4c98e
Rafa Gómez <rgomezcasas@gmail.com>
2020-02-07 18:13:40 +0100

    🔥 Add course service

--- src/mooc/main/tv/codely/mooc/courses/application/CourseService.java
+++ src/mooc/main/tv/codely/mooc/courses/application/CourseService.java
@@ -18,6 +18,10 @@ public class CourseService {
         this.eventBus   = eventBus;
     }
 
+
+
+
+
     public void create(CourseId id, CourseName name, CourseDuration duration) {
         Course course = Course.create(id, name, duration);
 
